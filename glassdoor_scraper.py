781dffc966f6cc0a2d8e3dbc1a76f3eab8a7ecbd
Mohammed Zwaib <46110270+Mohammed-Rwelly@users.noreply.github.com>
2022-09-30 14:18:58 -0700

    Update glassdoor_scraper.py

--- glassdoor_scraper.py
+++ glassdoor_scraper.py
@@ -9,3 +9,6 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 import albumentations as album
 import segmentation_models_pytorch as smp
+img=cv2.imread('4_5764945309327167165.jpg')
+image=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+plt.imshow(image)
